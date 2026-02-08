90acd811e040e28615e95a0ce41102b9ddca6ae3
hihat <hihat098@gmail.com>
2026-02-08 04:41:48 +0300

    fix supabase proxy for web

--- netlify/functions/supabase-proxy.js
+++ netlify/functions/supabase-proxy.js
@@ -1,18 +1,22 @@
-exports.handler = async (event) => {
+exports.handler = async () => {
   const SUPABASE_URL =
     "https://vpychjqluwggrhyqllbu.supabase.co/rest/v1/offers?select=*";
 
   const response = await fetch(SUPABASE_URL, {
     headers: {
-      apikey: process.env.SUPABASE_ANON_KEY,
-      Authorization: `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+      apikey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+      Authorization: `Bearer ${process.env.SUPABASE_SERVICE_ROLE_KEY}`,
     },
   });
 
-  const data = await response.json();
+  const data = await response.text();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: data,
   };
 };
